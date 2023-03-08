04bf74f49cbd788725ab43e477b638e5f9c4be0b
natan.rauber <natanrauber9911@gmail.com>
2023-03-07 21:28:50 -0300

    Create cow.lua

---
+++ data-otservbr-global/monster/mammals/cow.lua
@@ -0,0 +1,112 @@
+local mType = Game.createMonsterType("Cow")
+local monster = {}
+
+monster.description = "a cow"
+monster.experience = 0
+monster.outfit = {
+    lookType = 1253,
+    lookHead = 0,
+    lookBody = 0,
+    lookLegs = 0,
+    lookFeet = 0,
+    lookAddons = 0,
+    lookMount = 0
+}
+
+-- monster.raceId = 14
+-- monster.Bestiary = {
+-- 	class = "Mammal",
+-- 	race = BESTY_RACE_MAMMAL,
+-- 	toKill = 250,
+-- 	FirstUnlock = 10,
+-- 	SecondUnlock = 100,
+-- 	CharmsPoints = 5,
+-- 	Stars = 1,
+-- 	Occurrence = 0,
+-- 	Locations = "In Rookgaard Sheep field, Greenshore and other farms like the one in Thais, east of Carlin. They can also be found wandering the fields of Edron, and some can be found along with a Black Sheep with the Hunter in Femur Hills."
+-- }
+
+monster.health = 35
+monster.maxHealth = 35
+monster.race = "blood"
+monster.corpse = 36926
+monster.speed = 105
+monster.manaCost = 250
+monster.maxSummons = 0
+
+monster.changeTarget = {
+    interval = 4000,
+    chance = 0
+}
+
+monster.strategiesTarget = {
+    nearest = 100
+}
+
+monster.flags = {
+    summonable = true,
+    attackable = true,
+    hostile = false,
+    convinceable = true,
+    pushable = true,
+    rewardBoss = false,
+    illusionable = true,
+    canPushItems = false,
+    canPushCreatures = false,
+    staticAttackChance = 90,
+    targetDistance = 1,
+    runHealth = 35,
+    healthHidden = false,
+    isBlockable = false,
+    canWalkOnEnergy = false,
+    canWalkOnFire = false,
+    canWalkOnPoison = false,
+    pet = false
+}
+
+monster.light = {
+    level = 0,
+    color = 0
+}
+
+monster.voices = {
+    interval = 5000,
+    chance = 10,
+    {text = "Moo", yell = false}
+}
+
+monster.loot = {
+    {name = "ham", chance = 45000, maxCount = 2},
+    {name = "veal", chance = 45000, maxCount = 1}
+}
+
+monster.attacks = {
+    {name = "melee", interval = 2000, chance = 100, minDamage = 0, maxDamage = 0}
+}
+
+monster.defenses = {
+    defense = 8,
+    armor = 10
+}
+
+monster.elements = {
+    {type = COMBAT_PHYSICALDAMAGE, percent = 0},
+    {type = COMBAT_ENERGYDAMAGE, percent = 0},
+    {type = COMBAT_EARTHDAMAGE, percent = 0},
+    {type = COMBAT_FIREDAMAGE, percent = 0},
+    {type = COMBAT_LIFEDRAIN, percent = 0},
+    {type = COMBAT_MANADRAIN, percent = 0},
+    {type = COMBAT_DROWNDAMAGE, percent = 0},
+    {type = COMBAT_ICEDAMAGE, percent = 0},
+    {type = COMBAT_HOLYDAMAGE, percent = 0},
+    {type = COMBAT_DEATHDAMAGE, percent = 0}
+}
+
+monster.immunities = {
+    {type = "paralyze", condition = false},
+    {type = "outfit", condition = false},
+    {type = "invisible", condition = false},
+    {type = "bleed", condition = false}
+}
+
+mType:register(monster)
