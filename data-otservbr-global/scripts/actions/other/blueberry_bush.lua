d3d13b279e356e5f5de21b48f40ef6b71c3fec58
natan.rauber <natanrauber9911@gmail.com>
2023-03-12 12:00:26 -0300

    Update blueberry_bush.lua

--- data-otservbr-global/scripts/actions/other/blueberry_bush.lua
+++ data-otservbr-global/scripts/actions/other/blueberry_bush.lua
@@ -3,7 +3,9 @@ local blueberryBush = Action()
 function blueberryBush.onUse(player, item, fromPosition, target, toPosition, isHotkey)
 	item:transform(3700)
 	item:decay()
-	Game.createItem(3588, 3, fromPosition)
+	player:feed(3 * 15)
+	player:say("Yum.", TALKTYPE_MONSTER_SAY)
+	Game.createItem(3588, math.random(5), fromPosition)
 	player:addAchievementProgress('Bluebarian', 500)
 	return true
 end
