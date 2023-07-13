ad1960c7abd22bbd1cca648d1a2c65e50a7ce39b
natan.rauber <natanrauber9911@gmail.com>
2023-07-12 22:20:28 -0300

    Update save_interval.lua

--- data-otservbr-global/scripts/globalevents/customs/save_interval.lua
+++ data-otservbr-global/scripts/globalevents/customs/save_interval.lua
@@ -4,14 +4,14 @@ local function serverSave(interval)
 	end
 
 	saveServer()
-	local message = "Server save complete. Next save in %d %ss!"
-	local messageSingle = "Server save complete. Next save in %d %s!"
+	local message = "Server save complete."
+	local messageSingle = "Server save complete."
 	Webhook.send("Server save", message, WEBHOOK_COLOR_WARNING)
 	if SAVE_INTERVAL_CONFIG_TIME > 1 then
-		Game.broadcastMessage(string.format(message, SAVE_INTERVAL_CONFIG_TIME, SAVE_INTERVAL_TYPE), MESSAGE_GAME_HIGHLIGHT)
+		Game.broadcastMessage(string.format(message, SAVE_INTERVAL_CONFIG_TIME, SAVE_INTERVAL_TYPE), MESSAGE_STATUS)
 		Spdlog.info(string.format(message, SAVE_INTERVAL_CONFIG_TIME, SAVE_INTERVAL_TYPE))
 	else
-		Game.broadcastMessage(string.format(messageSingle, SAVE_INTERVAL_CONFIG_TIME, SAVE_INTERVAL_TYPE), MESSAGE_GAME_HIGHLIGHT)
+		Game.broadcastMessage(string.format(messageSingle, SAVE_INTERVAL_CONFIG_TIME, SAVE_INTERVAL_TYPE), MESSAGE_STATUS)
 		Spdlog.info(string.format(messageSingle, SAVE_INTERVAL_CONFIG_TIME, SAVE_INTERVAL_TYPE))
 	end
 end
@@ -21,9 +21,8 @@ local save = GlobalEvent("save")
 function save.onTime(interval)
 	local remainingTime = 60 * 1000
 	if configManager.getBoolean(configKeys.TOGGLE_SAVE_INTERVAL) then
-		local message = "The server will save all accounts within " .. (remainingTime / 1000) .. " seconds. \z
-		You might lag or freeze for 5 seconds, please find a safe place."
-		Game.broadcastMessage(message, MESSAGE_GAME_HIGHLIGHT)
+		local message = "Server save in " .. (remainingTime / 1000) .. " seconds."
+		Game.broadcastMessage(message, MESSAGE_STATUS)
 		Spdlog.info(string.format(message, SAVE_INTERVAL_CONFIG_TIME, SAVE_INTERVAL_TYPE))
 		addEvent(serverSave, remainingTime, interval)
 		return true
