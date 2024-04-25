9244aa84dc04cdbf3625c934e74fc23829f75c42
natan.rauber <natanrauber9911@gmail.com>
2024-04-25 12:39:36 -0300

    [Add] convert_trees.lua

---
+++ data-otservbr-global/startup/others/convert_trees.lua
@@ -0,0 +1,25 @@
+local trees = {
+    [3681] = {3699} -- blueberry bush
+    -- [3617] = {50144}, -- plum tree
+    -- [3619] = {50146} -- pear tree
+}
+
+-- will randomly convert some trees into harvestable ones
+function ConvertTrees()
+    local i = 0
+    for x = 30000, 35000 do
+        for y = 30000, 35000 do
+            local tile = Tile(Position(x, y, 7))
+            if tile then
+                for k, v in pairs(trees) do
+                    local tree = tile:getItemById(k)
+                    if tree and math.random(50) == 1 then
+                        tree:transform(v[1])
+                        i = i + 1
+                    end
+                end
+            end
+        end
+    end
+    Spdlog.info("Successfully converted " .. i .. " trees")
+end
--- data-otservbr-global/startup/others/load.lua
+++ data-otservbr-global/startup/others/load.lua
@@ -1,1 +1,3 @@
 dofile(DATA_DIRECTORY.. '/startup/others/functions.lua')
+dofile(DATA_DIRECTORY.. '/startup/others/convert_trees.lua')
+
