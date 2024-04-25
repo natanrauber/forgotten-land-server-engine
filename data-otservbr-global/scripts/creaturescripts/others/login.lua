2291148963265bb5f92d7481913c19d069ca5025
natan.rauber <natanrauber9911@gmail.com>
2024-04-25 10:51:47 -0300

    Update login.lua

--- data-otservbr-global/scripts/creaturescripts/others/login.lua
+++ data-otservbr-global/scripts/creaturescripts/others/login.lua
@@ -21,21 +21,22 @@ end
 local playerLogin = CreatureEvent("PlayerLogin")
 
 function playerLogin.onLogin(player)
-	local items = {
-		{3003, 1},
-		-- {3031, 3}
-	}
+	-- local items = {
+	-- 	{3003, 1},
+	-- 	{3270, 1},
+	-- 	{3561, 1}
+	-- }
 	if player:getLastLoginSaved() == 0 then
-		player:teleportTo({x = 31928, y=32258, z=7})
-		player:teleportTo({x = 31929, y=32258, z = 7})
+		player:teleportTo({x = 32042, y = 32199, z= 6})
+		player:teleportTo({x = 32042, y = 32198, z= 6})
 		player:sendOutfitWindow()
-		local backpack = player:addItem(2854)
-		if backpack then
-			for i = 1, #items do
-				backpack:addItem(items[i][1], items[i][2])
-			end
-		end
-		player:addItem(2920, 1, true, 1, CONST_SLOT_AMMO)
+		-- local backpack = player:addItem(2854)
+		-- if backpack then
+		-- 	for i = 1, #items do
+		-- 		backpack:addItem(items[i][1], items[i][2])
+		-- 	end
+		-- end
+		-- player:addItem(2920, 1, true, 1, CONST_SLOT_AMMO)
 		db.query("UPDATE `players` SET `istutorial` = 0 where `id`=" .. player:getGuid())
 		-- Open channels
 		if table.contains({TOWNS_LIST.DAWNPORT, TOWNS_LIST.DAWNPORT_TUTORIAL}, player:getTown():getId()) then
