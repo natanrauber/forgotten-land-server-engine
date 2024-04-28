95a500a732a5012b7706003c2660aa92c4d87482
natan.rauber <natanrauber9911@gmail.com>
2024-04-28 01:39:38 -0300

    Update twines_farm.lua

--- data-otservbr-global/scripts/actions/rookgaard/twines_farm.lua
+++ data-otservbr-global/scripts/actions/rookgaard/twines_farm.lua
@@ -1,9 +1,11 @@
 local twinesFarm = Action()
 
-local t = {x = 32021, y = 32182, z = 6}
+local t = {x = 32021, y = 32182, z = 5}
 
 function twinesFarm.onUse(cid, item, position, fromPosition)
-    doTeleportThing(cid, t)
+    if getPlayerPosition(cid).y == 32183 then
+        doTeleportThing(cid, position)
+    end
 end
 
 twinesFarm:aid(9985)
